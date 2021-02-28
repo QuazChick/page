c5a8b38e100ffa36d456166c493a1ae132724d87
QuazChick <79767058+QuazChick@users.noreply.github.com>
2021-02-28 16:35:25 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,4 @@
-if (localStorage.getItem(‘cookieSeen’) != ‘shown’) {
-  $(‘.cookie-banner’).delay(2000).fadeIn();
-  localStorage.setItem(‘cookieSeen’,’shown’)
-};
-$(‘.close’).click(function() {
-  $(‘.cookie-banner’).fadeOut();
-})
-(function() {
-  "use strict";
+
 
   /**
    * Easy selector helper function
